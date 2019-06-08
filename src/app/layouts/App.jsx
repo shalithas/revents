47dc302c45b12046bbaadc2eb2f040eb38941819
Shalitha Senarathne <shalitha89@gmail.com>
2019-06-09 00:06:01 +0800

    end of section 3

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/EventDeashboard/EventDashboard";
+import NavBar from "../../features/navBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vent</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
